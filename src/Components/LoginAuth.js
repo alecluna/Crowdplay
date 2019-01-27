646e178313667d46334537ca7476ce4a139b187f
Alec Luna <alecluna@csus.edu>
2019-01-26 23:26:39 -0800

    changed production callback for oauth

--- src/Components/LoginAuth.js
+++ src/Components/LoginAuth.js
@@ -129,7 +129,7 @@ class LoginAuth extends Component {
             onClick={() => {
               window.location = window.location.href.includes("localhost")
                 ? "http://localhost:8888/login"
-                : "https://prodURLgoesHERE.com"; //prod URL goes here
+                : "https://crowdplay-music-backend.herokuapp.com/callback"; //prod URL goes here
             }}
           >
             <Typography style={{ color: "white" }}>
