d68211ff73346e0b93ad9f6cc582773e75b69695
Josh Singh <jashmit04@yahoo.com>
2019-02-11 17:17:16 -0800

    Intial Firebase Work. Read messages from firebase

--- src/Components/JoinUsers.js
+++ src/Components/JoinUsers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Typography from "../../node_modules/@material-ui/core/Typography";
 import Header from "./Header";
 import Paper from "../../node_modules/@material-ui/core/Paper";
@@ -23,23 +23,57 @@ const styles = {
   }
 };
 
-const JoinUsers = () => (
-  <div>
-    <Header />
-    <div style={styles.background}>
-      <Paper style={styles.paperStyle} elevation={11}>
-        <div style={styles.centerStyling}>
-          <Typography
-            variant="display2"
-            style={{ color: "black", fontWeight: "200" }}
-          >
-            Join a Session
+
+class JoinUsers extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      userId: "userId1",
+      sessionId: "",
+      chatId: ""
+    }
+  }
+
+  componentDidMount() {
+    let userId = "userId1";
+    // fetch(`https://spotify-crowdplay.firebaseio.com/Users/${userId}.json`)
+    //   .then(sessionIds=>sessionIds.json())
+    //   .then(sessionIds=> sessionIds.key1)
+    //   .then(sessionId=>{
+    //     this.setState({sessionId:sessionId});
+    //     console.log(sessionId)
+    //   })
+    fetch(`https://spotify-crowdplay.firebaseio.com/Chats/chatsId.json`).then(resp => resp.json())
+      .then(messages => { this.setState({ messages: messages }) });
+
+  }
+
+  render() {
+    const { messages } = this.state;
+    console.log(messages)
+    return (
+      <div>
+        <Header />
+        <div style={styles.background}>
+          <Paper style={styles.paperStyle} elevation={11}>
+            <div style={styles.centerStyling}>
+              <Typography
+                variant="display2"
+                style={{ color: "black", fontWeight: "200" }}
+              >
+                Join a Session
           </Typography>
+            </div>
+            <div style={styles.centerStyling} />
+
+            {/* LIST OF MSGS */}
+            <p>{messages}</p>
+
+          </Paper>
         </div>
-        <div style={styles.centerStyling} />
-      </Paper>
-    </div>
-  </div>
-);
+      </div>)
+  }
+}
 
 export default JoinUsers;
