bfadb48bbf4144dd8ad0dd354f5d21fa5961c834
Alec Luna <alecluna@csus.edu>
2019-04-30 18:28:17 -0700

    remove chat, add persisted panel with relavant info

--- src/Components/Playlist/PlaylistSearch.js
+++ src/Components/Playlist/PlaylistSearch.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { TextField } from "@material-ui/core";
 import { Button } from "@material-ui/core";
-import uniqBy from "lodash/uniqBy";
 
 export default class PlaylistSearch extends Component {
   constructor(props) {
