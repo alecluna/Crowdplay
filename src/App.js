0f31bb1930bbaf27c13ae0433763d32f8072316b
Alec Luna <alecluna@csus.edu>
2019-07-20 02:20:22 -0700

    small changes, add env

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Routes from "./Routes";
 import "./App.css";
 import firebase from "firebase/app";
 import "firebase/firestore";
+require("dotenv");
 
 var firebaseConfig = {
-  apiKey: "AIzaSyA0_0t9zhunPshMwxhxeGIIdKBpz8FdFFw",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "spotify-crowdplay.firebaseapp.com",
   databaseURL: "https://spotify-crowdplay.firebaseio.com",
   projectId: "spotify-crowdplay",
-  storageBucket: "spotify-crowdplay.appspot.com",
-  messagingSenderId: "77255330009"
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_ID
 };
 firebase.initializeApp(firebaseConfig);
 
