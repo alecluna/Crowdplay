e470b3f961ab218cfd626d35882a50cedcf17017
alec.pallet <alec@pallet.click>
2020-07-31 17:21:28 -0700

    return loginAuth to class based component, remove hooks for now

--- src/Components/LoginAuth.js
+++ src/Components/LoginAuth.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import queryString from "query-string";
@@ -13,186 +13,189 @@ const styles = {
   },
 };
 
-const LoginAuth = () => {
-  const [data, setData] = useState({});
-  const [name, setName] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-  const [userID, setUserID] = useState("");
-  const [photoURL, setphotoURL] = useState([]);
-  const [error, setError] = useState(false);
+class LoginAuth extends Component {
+  constructor() {
+    super();
+    this.state = { name: "", accessToken: "", userID: "", photoURL: [] };
+  }
 
-  useEffect(async () => {
-    // parse new access token from window location
+  componentDidMount() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
 
-    if (!accessToken) return;
-
-    try {
-      const response = await fetch("https://api.spotify.com/v1/me", {
-        headers: { Authorization: "Bearer " + accessToken },
-      });
-
-      const responseData = await response.json();
-      setData({ ...responseData });
-
-      if (data) {
-        const { images } = data;
-        if (images && images.length > 0) setphotoURL(images[0].url);
-
-        setName(data.display_name);
-        setAccessToken(accessToken);
-        setUserID(data.id);
-
+    this.getSpotifyData(accessToken)
+      .then((data) => {
+        if (data) {
+          if (data.images && data.images.length) {
+            this.setState({
+              photoURL: data.images[0].url,
+            });
+          }
+          this.setState({
+            name: data.display_name,
+            accessToken: accessToken,
+            userID: data.id,
+          });
+        }
+      })
+      .then(() => {
         let firestore = firebase.firestore();
-
         firebase
           .firestore()
           .collection("users")
-          .doc(userID)
+          .doc(this.state.userID)
           .get()
           .then((doc) => {
             if (!doc.exists) {
               //this should occur in account creation. probably in backend
               firestore.collection("users").doc(this.state.userID).set({
-                name,
-                userID,
-                photoURL,
+                name: this.state.name,
+                userID: this.state.userID,
+                photoURL: this.state.photoURL,
               });
             }
           });
-        console.log("Welcome! user_ID:  " + userID);
-      }
-    } catch (e) {
-      console.log("error", e);
-      setError(true);
-    }
-  }, []);
+      })
+      .catch((error) => console.log("error: ", error));
+  }
 
-  const getStateObj = () => {
-    return {
-      name,
-      accessToken,
-      userID,
-      photoURL,
-    };
-  };
-  return (
-    <div style={styles.background}>
-      {error && <p>error connecting to Spotify</p>}
-      {name ? (
-        <div style={{ width: "75%" }}>
-          <Typography
-            style={{
-              justifyContent: "center",
-              fontWeight: "200",
-              textAlign: "center",
-            }}
-            variant="h6"
-            color="default"
-          >
-            Welcome {name}!
-          </Typography>
+  async getSpotifyData(accessToken) {
+    const response = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
 
+    return data;
+  }
+
+  render() {
+    const { name, accessToken, userID, photoURL } = this.state;
+    return (
+      <div style={styles.background}>
+        {name ? (
+          <div style={{ width: "75%" }}>
+            <Typography
+              style={{
+                justifyContent: "center",
+                fontWeight: "200",
+                textAlign: "center",
+              }}
+              variant="h6"
+              color="default"
+            >
+              Welcome {name}!
+            </Typography>
+
+            <Spring
+              from={{ opacity: 0, transform: "translate3d(0,90px,0)" }}
+              to={{ opacity: 1, transform: "translate3d(0,0px,0)" }}
+            >
+              {(props) => (
+                <div style={props}>
+                  <div
+                    style={{
+                      marginTop: "50px",
+                      display: "flex",
+                      justifyContent: "space-around",
+                    }}
+                  >
+                    <Button
+                      variant="contained"
+                      style={{
+                        margin: "10px",
+                        background: "#1db954",
+                        borderRadius: "20px",
+                      }}
+                    >
+                      <Link
+                        style={{ textDecoration: "none " }}
+                        to={{
+                          pathname: "/createsession",
+                          state: {
+                            name: name,
+                            accessToken: accessToken,
+                            userID: userID,
+                            photoURL: photoURL,
+                          },
+                        }}
+                      >
+                        <Typography style={{ color: "white" }}>
+                          Create a Session
+                        </Typography>
+                      </Link>
+                    </Button>
+
+                    <Button
+                      variant="contained"
+                      style={{
+                        margin: "10px",
+                        background: "#1db954",
+                        borderRadius: "20px",
+                      }}
+                    >
+                      <Link
+                        style={{ textDecoration: "none " }}
+                        to={{
+                          pathname: "/joinusers",
+                          state: {
+                            name: name,
+                            accessToken: accessToken,
+                            userID: userID,
+                            photoURL: photoURL,
+                          },
+                        }}
+                      >
+                        <Typography style={{ color: "white" }}>
+                          View your Sessions
+                        </Typography>
+                      </Link>
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </Spring>
+          </div>
+        ) : (
           <Spring
             from={{ opacity: 0, transform: "translate3d(0,90px,0)" }}
             to={{ opacity: 1, transform: "translate3d(0,0px,0)" }}
           >
             {(props) => (
               <div style={props}>
-                <div
+                <Button
+                  variant="contained"
                   style={{
-                    marginTop: "50px",
-                    display: "flex",
-                    justifyContent: "space-around",
+                    background: "#1db954",
+                    borderRadius: "20px",
+                  }}
+                  onClick={() => {
+                    window.location = window.location.href.includes("localhost")
+                      ? "http://localhost:8888/login"
+                      : "https://crowdplay-music-backend.herokuapp.com/login"; //prod URL here
                   }}
                 >
-                  <Button
-                    variant="contained"
+                  <Typography
                     style={{
-                      margin: "10px",
-                      background: "#1db954",
-                      borderRadius: "20px",
+                      color: "white",
+                      width: "10rem",
+                      fontSize: "2em",
+                      textAlign: "center",
                     }}
                   >
-                    <Link
-                      style={{ textDecoration: "none " }}
-                      to={{
-                        pathname: "/createsession",
-                        state: {
-                          ...getStateObj(),
-                        },
-                      }}
-                    >
-                      <Typography style={{ color: "white" }}>
-                        Create a Session
-                      </Typography>
-                    </Link>
-                  </Button>
-
-                  <Button
-                    variant="contained"
-                    style={{
-                      margin: "10px",
-                      background: "#1db954",
-                      borderRadius: "20px",
-                    }}
-                  >
-                    <Link
-                      style={{ textDecoration: "none " }}
-                      to={{
-                        pathname: "/joinusers",
-                        state: {
-                          ...getStateObj(),
-                        },
-                      }}
-                    >
-                      <Typography style={{ color: "white" }}>
-                        View your Sessions
-                      </Typography>
-                    </Link>
-                  </Button>
-                </div>
+                    Login
+                  </Typography>
+                </Button>
               </div>
             )}
           </Spring>
-        </div>
-      ) : (
-        <Spring
-          from={{ opacity: 0, transform: "translate3d(0,90px,0)" }}
-          to={{ opacity: 1, transform: "translate3d(0,0px,0)" }}
-        >
-          {(props) => (
-            <div style={props}>
-              <Button
-                variant="contained"
-                style={{
-                  background: "#1db954",
-                  borderRadius: "20px",
-                }}
-                onClick={() => {
-                  window.location = window.location.href.includes("localhost")
-                    ? "http://localhost:8888/login"
-                    : "https://crowdplay-music-backend.herokuapp.com/login"; //prod URL here
-                }}
-              >
-                <Typography
-                  style={{
-                    color: "white",
-                    width: "10rem",
-                    fontSize: "2em",
-                    textAlign: "center",
-                  }}
-                >
-                  Login
-                </Typography>
-              </Button>
-            </div>
-          )}
-        </Spring>
-      )}
-    </div>
-  );
-};
+        )}
+      </div>
+    );
+  }
+}
 
 export default LoginAuth;
